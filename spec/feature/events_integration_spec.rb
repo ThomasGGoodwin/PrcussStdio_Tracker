f420338d8ae4b437b557275f4c43926baaa69b0e
Steven Grimshaw <stevengrimshaw@tamu.edu>
2022-02-14 21:02:36 -0600

    Testing for events. Modifications to OAuth to allow testing.

---
+++ spec/feature/events_integration_spec.rb
@@ -0,0 +1,133 @@
+# location: spec/feature/events_integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating an event', type: :feature do
+  scenario 'valid inputs' do
+    # Login to Google
+    visit '/admins/auth/google_oauth2'
+
+    # Create new event
+    visit new_event_path
+    fill_in 'Event type', with: 1
+    fill_in 'Name', with: 'EventTest'
+    select '2022', :from => 'event_start_time_1i'
+    select 'February', :from => 'event_start_time_2i'
+    select '17', :from => 'event_start_time_3i'
+    select '20', :from => 'event_start_time_4i'
+    select '19', :from => 'event_start_time_5i'
+    select '2023', :from => 'event_end_time_1i'
+    select 'April', :from => 'event_end_time_2i'
+    select '14', :from => 'event_end_time_3i'
+    select '01', :from => 'event_end_time_4i'
+    select '00', :from => 'event_end_time_5i'
+    fill_in 'Location', with: 'LocationTest'
+    fill_in 'Description', with: 'DescriptionTest'
+    click_on 'Create Event'
+
+    # Check page for created event
+    visit events_path
+    expect(page).to have_content('1')
+    expect(page).to have_content('EventTest')
+    expect(page).to have_content('2022-02-17 20:19:00')
+    expect(page).to have_content('2023-04-14 01:00:00')
+    expect(page).to have_content('LocationTest')
+    expect(page).to have_content('DescriptionTest')
+  end
+end
+
+RSpec.describe 'Editing an event', type: :feature do
+
+  let!(:event) { Event.create(event_type: 1, name: 'EventTest', start_time: '2022-02-17 01:01:00', end_time: '2022-02-17 02:01:00', location: 'LocationTest', description: 'DescriptionTest') }
+
+  scenario 'valid inputs' do
+    # Login to Google
+    visit '/admins/auth/google_oauth2'
+
+    # Edit new event
+    visit events_path
+    click_on 'Edit'
+    fill_in 'Event type', with: 2
+    fill_in 'Name', with: 'EventTest2'
+    select '2023', :from => 'event_start_time_1i'
+    select 'April', :from => 'event_start_time_2i'
+    select '14', :from => 'event_start_time_3i'
+    select '01', :from => 'event_start_time_4i'
+    select '01', :from => 'event_start_time_5i'
+    select '2021', :from => 'event_end_time_1i'
+    select 'February', :from => 'event_end_time_2i'
+    select '17', :from => 'event_end_time_3i'
+    select '00', :from => 'event_end_time_4i'
+    select '01', :from => 'event_end_time_5i'
+    fill_in 'Location', with: 'LocationTest2'
+    fill_in 'Description', with: 'DescriptionTest2'
+    click_on 'Update Event'
+
+    # Check page for created event
+    visit events_path
+    expect(page).to have_content('2')
+    expect(page).to have_content('EventTest2')
+    expect(page).to have_content('2023-04-14 01:01:00')
+    expect(page).to have_content('2021-02-17 00:01:00')
+    expect(page).to have_content('LocationTest2')
+    expect(page).to have_content('DescriptionTest2')
+  end
+
+  scenario 'empty inputs' do
+    # Login to Google
+    visit '/admins/auth/google_oauth2'
+
+    # Edit new event
+    visit events_path
+    click_on 'Edit'
+    fill_in 'Name', with: ''
+    fill_in 'Location', with: ''
+    fill_in 'Description', with: ''
+    click_on 'Update Event'
+
+    # Check for error messages
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Location can't be blank")
+    expect(page).to have_content("Description can't be blank")
+    
+
+    # Check page for created event
+    visit events_path
+    expect(page).to have_content('EventTest')
+    expect(page).to have_content('LocationTest')
+    expect(page).to have_content('DescriptionTest')
+  end
+end
+
+RSpec.describe 'Deleting an event', type: :feature do
+  scenario 'valid inputs' do
+    # Login to Google
+    visit '/admins/auth/google_oauth2'
+
+    # Create new event
+    visit new_event_path
+    fill_in 'Event type', with: 1
+    fill_in 'Name', with: 'EventTest'
+    select '2022', :from => 'event_start_time_1i'
+    select 'February', :from => 'event_start_time_2i'
+    select '17', :from => 'event_start_time_3i'
+    select '20', :from => 'event_start_time_4i'
+    select '19', :from => 'event_start_time_5i'
+    select '2023', :from => 'event_end_time_1i'
+    select 'April', :from => 'event_end_time_2i'
+    select '14', :from => 'event_end_time_3i'
+    select '01', :from => 'event_end_time_4i'
+    select '00', :from => 'event_end_time_5i'
+    fill_in 'Location', with: 'LocationTest'
+    fill_in 'Description', with: 'DescriptionTest'
+    click_on 'Create Event'
+
+    # Delete new event
+    visit events_path
+    click_on 'Destroy'
+
+    # Check page for created event
+    expect(page).to have_no_content('EventTest')
+    expect(page).to have_no_content('LocationTest')
+    expect(page).to have_no_content('DescriptionTest')
+  end
+end
