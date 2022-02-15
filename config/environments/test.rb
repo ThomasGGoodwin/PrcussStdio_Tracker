3c6e4be91f7d925cbf45cd11ff4886d07a41989e
Steven Grimshaw <stevengrimshaw@tamu.edu>
2022-02-14 22:55:53 -0600

    removed unnecessary environment var in test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,7 +61,4 @@ Rails.application.configure do
   # Google OAuth Keys
   ENV['GOOGLE_OAUTH_CLIENT_ID']  = '668612256365-uq3g4qq4e6c98hmo0gbj33mkajiv7cvm.apps.googleusercontent.com'
   ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
-
-  ENV["devise.mapping"] = Devise.mappings[:user]
-  ENV["omniauth.auth"]  = OmniAuth.config.mock_auth[:google_oauth2]
 end
