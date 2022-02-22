ccd89cef6cb318dc863789266800f309606eaa0a
root <root@localhost>
2022-02-22 14:35:20 +0000

    Controller logic for upcoming and past events, with view implementation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,8 @@ class EventsController < ApplicationController
   # GET /events or /events.json
   def index
     @events = Event.all
+    @upcoming_events = Event.where("end_time < ?", DateTime.now.utc) || [] 
+    @past_events = Event.where("end_time >= ?", DateTime.now.utc) || []
   end
 
   # GET /events/1 or /events/1.json
