2f7a8d0b4833205b721c41198ee82cc6fe438253
nassef99 <nassefameen@gmail.com>
2022-03-02 11:58:44 -0600

    updated integration test with empty case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, allow_blank: false
+    validates :last_name, presence: true, allow_blank: false
+    validates :email, presence: true, allow_blank: false
+    validates :role, presence: true, allow_blank: false
 end
--- spec/feature/attendance_report_integration_spec.rb
+++ spec/feature/attendance_report_integration_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe 'Viewing the report with a created User', type: :feature do
     
-   # let(:test_user) { User.new(first_name: 'Nassef', last_name: 'Ameen', email: 'nameen1@tamu.edu', role: 1)}
+    let!(:user) { User.create(first_name: 'Nassef', last_name: 'Ameen', email: 'nameen1@tamu.edu', role: 1)}
 
    scenario 'valid inputs' do
     # Login to Google
@@ -18,11 +18,38 @@ RSpec.describe 'Viewing the report with a created User', type: :feature do
     click_on 'Create User'
 
     # visit attendance report page
-    visit '/users/attendance_report'
+    visit attendance_report_path
 
     # check page to have test user information
     expect(page).to have_content('Harry')
     expect(page).to have_content('Potter')
+    expect(page).to have_content(2)
+    expect(page).to have_content('No')
+   end
+
+   scenario "empty inputs" do
+    # Login to Google
+    visit '/admins/auth/google_oauth2'
+
+    # Edit created user
+    visit users_path
+    click_on 'Edit'
+    fill_in 'First name', with: ''
+    fill_in 'Last name', with: ''
+    fill_in 'Email', with: ''
+    fill_in 'Role', with: ''
+    click_on 'Update User'
+
+    # Check for error messages
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Role can't be blank")
+
+    # Check page for created User
+    visit attendance_report_path
+    expect(page).to have_content('Nassef')
+    expect(page).to have_content('Ameen')
     expect(page).to have_content(2)
     expect(page).to have_content('No')
    end
