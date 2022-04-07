af4e981f72c57dd6242b8f9db5bff2d7f0fdf477
Thomas Goodwin <thomas.gabriel.goodwin@gmail.com>
2022-04-07 15:32:13 -0500

    Revert "Moving environment variables to local_env.yml, removing them completely from GitHub."
    This reverts commit 729666eff63147ae2112f4a0160ccac0613a2d52.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,6 +76,10 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # Google OAuth Keys
+  ENV['GOOGLE_OAUTH_CLIENT_ID']  = '668612256365-uq3g4qq4e6c98hmo0gbj33mkajiv7cvm.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
+
   # For Local Testing
   config.web_console.permissions = '172.17.0.1'
 end
