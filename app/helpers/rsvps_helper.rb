a9bb9bf98519e18f713500319784714c9a4416fa
wyattsmith2023 <57771792+wyattsmith2023@users.noreply.github.com>
2022-04-08 17:44:25 -0500

    Add styling to rsvp page with separate logic in helper

--- app/helpers/rsvps_helper.rb
+++ app/helpers/rsvps_helper.rb
@@ -9,4 +9,30 @@ module RsvpsHelper
         end
         content.html_safe
     end
+
+    def verify_creds_rsvp(role)
+        if role != 'Admin'
+            render 'shared_partials/invalid_access'
+        else
+            render 'index'
+        end
+    end
+
+    #select the approriate navbar based on role
+    def rsvp_navbar(role)
+        if role == 'Admin'
+            render 'shared_partials/admin_nav'
+        elsif role == 'Member'
+            render 'shared_partials/member_nav'
+        end
+    end
+
+    #change exit point based on role
+    def rsvp_back(role)
+        if role == 'Admin'
+            link_to 'Back', rsvps_path, class:'btn btn-secondary a-primary'
+        elsif role =='Member'
+            link_to 'Back', root_url, class:'btn btn-secondary a-primary'
+        end
+    end
 end
