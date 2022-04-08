2600d947fb7ae2aeb75eaff2ca217815a5e88741
wyattsmith2023 <57771792+wyattsmith2023@users.noreply.github.com>
2022-04-08 13:33:39 -0500

    Changes to all event types pages to have logic in the helper and controller

--- app/helpers/event_types_helper.rb
+++ app/helpers/event_types_helper.rb
@@ -1,2 +1,16 @@
 module EventTypesHelper
+    def verify_creds_eventtypes(role, page)
+        role = 'Member'
+        if role != 'Admin'
+            render 'shared_partials/invalid_access'
+        elsif page == 'index'
+            render 'index'
+        elsif page == 'edit'
+            render 'edit'
+        elsif page == 'show'
+            render 'show'
+        elsif page == 'new'
+            render 'new'
+        end
+    end
 end
