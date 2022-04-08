22f9e1e1a55fc21f8bd94e0c6dbb548b909cc36c
wyattsmith2023 <57771792+wyattsmith2023@users.noreply.github.com>
2022-04-08 13:51:16 -0500

    Changes to all roles pages to have logic in the helper and controller

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,2 +1,16 @@
 module RolesHelper
+    def verify_creds_roles(role, page)
+        #role = 'Member'
+        if role != 'Admin'
+            render 'shared_partials/invalid_access'
+        elsif page == 'index'
+            render 'index'
+        elsif page == 'edit'
+            render 'edit'
+        elsif page == 'show'
+            render 'show'
+        elsif page == 'new'
+            render 'new'
+        end
+    end
 end
