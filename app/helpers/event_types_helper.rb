d51d16c0e34b0e1d0b887aab7b83cfd34d21d1c4
wyattsmith2023 <57771792+wyattsmith2023@users.noreply.github.com>
2022-04-08 13:39:46 -0500

    Remove debug line

--- app/helpers/event_types_helper.rb
+++ app/helpers/event_types_helper.rb
@@ -1,6 +1,5 @@
 module EventTypesHelper
     def verify_creds_eventtypes(role, page)
-        role = 'Member'
         if role != 'Admin'
             render 'shared_partials/invalid_access'
         elsif page == 'index'
