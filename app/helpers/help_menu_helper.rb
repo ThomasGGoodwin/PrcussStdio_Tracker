e131dfd83257d1a6ffc13226323d384c975baa60
Steven Grimshaw <stevengrimshaw@tamu.edu>
2022-04-11 22:29:41 -0500

    merge fix

--- app/helpers/help_menu_helper.rb
+++ app/helpers/help_menu_helper.rb
@@ -1,4 +1,12 @@
 module HelpMenuHelper
+    def check_navbar(role)
+        if role == 'Admin'
+            render 'shared_partials/admin_nav'
+        elsif role == 'Member'
+            render 'shared_partials/member_nav'
+        end
+    end
+
     def check_privilege(role)
         if role == 'Admin'
             render 'admin_view'
